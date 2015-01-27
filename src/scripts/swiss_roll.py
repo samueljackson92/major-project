280375ffd71cc6a94a7315bcc000f59a9e9d3328
Samuel Jackson <samueljackson@outlook.com>
2015-01-27 16:38:23 +0000

    Playing with Isomap.
    Varying the value of k on the swiss roll dataset and visualising the
    error.

--- src/scripts/swiss_roll.py
+++ src/scripts/swiss_roll.py
@@ -3,32 +3,40 @@ from sklearn import manifold
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_3D(X):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+def add_sub_plot(fig, index, data, t, proj=None):
+    ax = fig.add_subplot(index, projection=proj)
 
-    ax.scatter(*X.T, marker='o')
-
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-
-    plt.show()
-
-def plot_2D(X):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-
-    ax.scatter(*X.T, marker='o')
+    ax.scatter(*data.T, marker='o', c=t, cmap=plt.cm.Spectral)
 
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
-
-    plt.show()
+    if proj is '3d':
+        ax.set_zlabel('Z Label')
 
 if __name__ == "__main__":
     X, t = make_swiss_roll(n_samples=1000)
-    Y = manifold.Isomap(10, 2).fit_transform(X)
-    plot_3D(X)
-    plot_2D(Y)
+
+    reconstruction_error = []
+    for i in range(2,21):
+        isomap = manifold.Isomap(i, 2)
+        Y = isomap.fit_transform(X)
+        reconstruction_error.append(isomap.reconstruction_error())
+
+    #Find embedding with the lowest reconstruction error.
+    best_fit = np.argmin(reconstruction_error)
+    print "%d: %f" % (best_fit, reconstruction_error[best_fit])
+
+    isomap = manifold.Isomap(best_fit, 2)
+    Y = isomap.fit_transform(X)
+
+    #plot reconstruction error rate over all trials
+    plt.plot(reconstruction_error)
+    plt.show()
+
+    #Plot swiss roll manifold and lower dimensional mapping
+    fig = plt.figure()
+    add_sub_plot(fig, 121, X, t, proj='3d')
+    add_sub_plot(fig, 122, Y, t)
+    plt.show()

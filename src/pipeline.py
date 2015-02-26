e18ea6578b5bf9caf32bcdf0d5fbea88457dea7f
Samuel Jackson <samueljackson@outlook.com>
2015-02-26 12:56:04 +0000

    Refs #18 Fix implementation of multifile reduction.

--- src/pipeline.py
+++ src/pipeline.py
@@ -14,6 +14,7 @@ Options:
 """
 import os.path
 import logging
+import time
 import numpy as np
 import pandas as pd
 
@@ -29,34 +30,42 @@ logger = logging.getLogger("MIA Pipeline")
 
 def process_blobs(image_dir, mask_dir, output_directory, scale_to_mask=False):
     img_names = []
+    features = np.empty((0,5))
     for image_path, mask_path in iterate_directory(image_dir, mask_dir):
-        features = np.empty((0,5))
 
         img_name = os.path.basename(image_path)
         img_names.append(img_name)
 
         logger.info("Processing image %s" % img_name)
 
+        start = time.time()
         img, msk = preprocess_image(image_path, mask_path,
                                     scale_to_mask=scale_to_mask)
 
         b = blob_detection(img, msk)
+        end = time.time()
+
         props = blob_props(b)
         props = props.reshape(1, props.size)
         features = np.vstack([features, props])
 
         logger.info("%d blobs found in image" % b.shape[0])
+        logger.debug("%.2f seconds to process" % (end-start))
 
-        column_names = ['blob_count', 'mean_radius', 'std_radius',
-                        'min_radius', 'max_radius']
-        feature_matrix = pd.DataFrame(features,
-                                      index = img_names,
-                                      columns=column_names)
-        feature_matrix.index.name = 'image_name'
-        with open(output_directory, 'r') as f:
-            feature_matrix.to_csv(f, output_directory, Header=False)
-
+    feature_matrix = create_feature_matrix(features, img_names)
     return feature_matrix
+
+def create_feature_matrix(features, img_names):
+    column_names = ['blob_count', 'mean_radius', 'std_radius',
+                    'min_radius', 'max_radius']
+
+    feature_matrix = pd.DataFrame(features,
+                                  index=img_names,
+                                  columns=column_names)
+    feature_matrix.index.name = 'image_name'
+    feature_matrix.to_csv(output_directory, Header=False)
+    return feature_matrix
+
 
 def process_image(img_path, mask_path):
     img, msk = preprocess_image(img_path, mask_path)
@@ -67,6 +76,7 @@ def process_image(img_path, mask_path):
     logger.debug(end-start)
     plot_blobs(img, blobs)
 
+
 def main():
     arguments = docopt(__doc__, version='0.4.0')
     image_dir = arguments["IMAGE"]
@@ -76,10 +86,9 @@ def main():
     if arguments['--verbose']:
         logger.setLevel(logging.DEBUG)
 
-    process_image(image_dir, mask_dir)
-
-    # feature_matrix = process_blobs(image_dir, mask_dir, output_directory,
-    #                                arguments['--scale-to-mask'])
+    # process_image(image_dir, mask_dir)
+    feature_matrix = process_blobs(image_dir, mask_dir, output_directory,
+                                   arguments['--scale-to-mask'])
 
 if __name__ == "__main__":
     main()

23ac289a2c45db97f66f7ae4cf61c4584b455d3c
Samuel Jackson <samueljackson@outlook.com>
2015-03-04 09:41:03 +0000

    Refs #30 Structure module components.

---
+++ src/mia/features/_nonmaximum_suppression.py
@@ -0,0 +1,76 @@
+"""
+Non-maximum suppression algorithm for selecting the most prominant line
+orinetation from a line strength image.
+
+Reference: Reyer Zwiggelaar, Tim C. Parr, and Christopher J. Taylor.
+"Finding Orientated Line Patterns in Digital Mammographic Images." BMVC. 1996.
+"""
+import numpy as np
+from scipy.ndimage import filters
+
+__all__ = ['nonmaximum_suppression']
+
+def nonmaximum_suppression(line_strength, line_orientation, nbins, kernel_size=3):
+    """Non-maximum suppression of the line strength images.
+
+    :param line_strength: the line strength image to process
+    :param line_orientation: the line orientation image to use
+    :param nbins: number of bins used to create the line_orientation image
+    :param kernel_size: size of the kernel neighbourhood (default 3)
+    :returns: ndarray -- the suppressed line strength image
+    """
+    if (nbins % 4) != 0:
+        raise ValueError("nbins must be a multiple of 4")
+
+    kernels = generate_kernels(kernel_size)
+
+    #Convert the line orientation image to the parallel orientation
+    parallel_orientation = (line_orientation + (nbins/2)) % nbins
+    parallel_orientation = parallel_orientation % len(kernels)
+
+    return filter_for_maximum_direction(kernels, line_strength, parallel_orientation)
+
+
+def filter_for_maximum_direction(kernels, line_strength, parallel_orientation):
+    """Filter the line_strength image for the maximum local direction
+
+    :param kernels: the list of kernels to filter the image
+    :param line_strength: the line strength image to process
+    :param parallel_orientation: the parallel orientation image to the original
+                                 line orientation image
+    :returns: ndarray -- the suppressed line strength image
+    """
+    filtered_images = []
+    for kernel in kernels:
+        filtered_image = np.zeros(line_strength.shape)
+        filters.maximum_filter(line_strength, footprint=kernel, output=filtered_image)
+        filtered_images.append(filtered_image)
+
+    line_strength_suppressed = np.zeros(line_strength.shape)
+    np.choose(parallel_orientation, filtered_images, out=line_strength_suppressed)
+    return line_strength_suppressed
+
+
+def generate_kernels(kernel_size):
+    """ Make 4 kernels in the horizontal, vertical and diagonal directions.
+
+    :param kernel_size: the size of the kernels to create.
+    :returns: list -- the generated kernels
+    """
+    horizontal = np.zeros(shape=(kernel_size,kernel_size), dtype='int64')
+    horizontal[1] = np.ones(kernel_size)
+
+    vertical = np.zeros(shape=(kernel_size,kernel_size), dtype='int64')
+    vertical[:,1] = np.ones(kernel_size, dtype='int64')
+
+    left_diagonal = np.eye(kernel_size, dtype='int64')
+    right_diagonal = np.fliplr(left_diagonal)
+
+    kernels = np.array([
+        np.where(horizontal),
+        np.where(left_diagonal),
+        np.where(vertical),
+        np.where(right_diagonal)
+    ])
+
+    return kernels
---
+++ src/mia/features/linear_structure.py
@@ -0,0 +1,38 @@
+from skimage import measure
+
+from mia._orientated_bins import orientated_bins
+from mia._nonmaximum_suppression import nonmaximum_suppression
+from mia.utils import *
+
+def linear_features(img, radius, nbins, threshold):
+    """Compute linear features from an image
+
+    Uses orientated bins with nonmaximum suppression and binary thinning.
+
+    :param img: image to find linear features in
+    :param radius: radius of the bins to use
+    :param nbins: number of bins to use
+    :returns: tuple -- containing (line_image, regions)
+    """
+    line_strength, line_orientation = orientated_bins(img, radius, nbins=nbins)
+    line_strength_suppressed = nonmaximum_suppression(line_strength, line_orientation, nbins)
+
+    line_image = binary_image(line_strength_suppressed, threshold)
+    line_image = skeletonize_image(line_image, 50, dilation_size=1)
+
+    #find image regions
+    line_image = measure.label(line_image)
+    regions = measure.regionprops(line_image)
+
+    return line_strength_suppressed, regions
+
+def extract_feature(props, image):
+    """ Extract the area of an image belonging to a feature given a bounding box
+
+    :param props: the properties of the region
+    :param image: image to extract the region from
+    :returns: ndarray -- section of the image within the bounding box
+    """
+    hs, ws, he, we = props.bbox
+    image_section = image[hs:he,ws:we]
+    return image_section

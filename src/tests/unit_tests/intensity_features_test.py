ce796387d84d94286a5b9bc40ea3386137ec50fc
Samuel Jackson <samueljackson@outlook.com>
2015-03-14 16:55:50 +0000

    Fix broken test.

--- src/tests/unit_tests/intensity_features_test.py
+++ src/tests/unit_tests/intensity_features_test.py
@@ -1,10 +1,10 @@
 import unittest
-import nose.tools
-import numpy.testing
+import numpy as np
 
 from mia.features.intensity import _intensity_stats
-from mia.utils import *
+from mia.utils import preprocess_image
 from ..test_utils import get_file_path
+
 
 class IntensityTests(unittest.TestCase):
 
@@ -14,7 +14,6 @@ class IntensityTests(unittest.TestCase):
         cls._img, msk = preprocess_image(img_path)
 
     def test_intensity_stats(self):
-        expected_output = np.array([222.22222222, 286.25310392,
-                                    1.33091438, 0.75751295])
+        expected_output = np.array([39.0625, 146.827585, 4.559662, 21.860318])
         stats = _intensity_stats(self._img)
         np.testing.assert_allclose(stats, expected_output)

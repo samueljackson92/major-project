9c9e048280d9fdb728b041ed0b57af631496772f
Samuel Jackson <samueljackson@outlook.com>
2015-03-21 16:50:30 +0000

    Refs #31 Increase coverage in blob detection tests.

--- src/tests/unit_tests/blob_detection_test.py
+++ src/tests/unit_tests/blob_detection_test.py
@@ -1,8 +1,7 @@
 import nose.tools
 import pandas as pd
 import unittest
-
-from mia.features.blobs import _blob_density
+from mia.features.blobs import _blob_density, blob_props
 from ..test_utils import *
 
 
@@ -20,3 +19,14 @@ class BlobDetectionTests(unittest.TestCase):
     def test_blob_density_measure(self):
         density = _blob_density(self._blobs[['x', 'y']], 4)
         nose.tools.assert_equal(density.shape, (self._blobs.shape[0],))
+
+    def test_blob_props(self):
+        props = blob_props(self._blobs)
+
+        nose.tools.assert_true(isinstance(props, pd.DataFrame))
+        nose.tools.assert_equal(props.shape, (1, 12))
+
+        ref_result_path = "reference_results/2015-03-05-results-blobs.csv"
+        ref_result = pd.DataFrame.from_csv(get_file_path(ref_result_path))
+
+        pd.util.testing.assert_frame_equal(props, ref_result)

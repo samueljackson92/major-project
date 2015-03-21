6a7b7ecc7c9e6b5ed92379cab5644c5972a1b716
Samuel Jackson <samueljackson@outlook.com>
2015-03-21 13:40:46 +0000

    Refs #31 Bump tolerance and fix some PEP8 warnings.

--- src/tests/unit_tests/texture_features_test.py
+++ src/tests/unit_tests/texture_features_test.py
@@ -15,13 +15,13 @@ class TextureTests(unittest.TestCase):
 
     def test_gabor_bank_features(self):
         orientations = np.arange(0, np.pi, np.pi/8)
-        frequencies = np.arange(0.1,5.0)
+        frequencies = np.arange(0.1, 5.0)
 
         gabor_magnitudes = gabor_features(self._img, frequencies, orientations)
         stats = image_orthotope_statistics(gabor_magnitudes)
 
-        nose.tools.assert_equals(gabor_magnitudes.shape, (5,8,100,100))
-        nose.tools.assert_equals(stats.shape, (3,5))
+        nose.tools.assert_equals(gabor_magnitudes.shape, (5, 8, 100, 100))
+        nose.tools.assert_equals(stats.shape, (3, 5))
 
         results_path = get_file_path('reference_results/tex1_gabor_features.npy')
         expected_stats = np.load(results_path)
@@ -30,10 +30,10 @@ class TextureTests(unittest.TestCase):
     def test_glcm_features(self):
         properties = ['contrast', 'dissimilarity']
         orientations = np.arange(0, np.pi, np.pi/8)
-        distances = [1,2,4,8]
+        distances = [1, 2, 4, 8]
 
         features = glcm_features(self._img, distances, orientations, properties)
-        nose.tools.assert_equals(features.shape, (2,4,8))
+        nose.tools.assert_equals(features.shape, (2, 4, 8))
 
         results_path = get_file_path('reference_results/tex1_glcm_features.npy')
         expected_result = np.load(results_path)

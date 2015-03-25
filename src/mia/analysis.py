0e2ad1747e743edf4bc591158325ddd27d4fb8fe
Samuel Jackson <samueljackson@outlook.com>
2015-03-25 13:01:38 +0000

    Refs #55 Clean up API for loading meta data for files.
    This includes better support for synthetics.

--- src/mia/analysis.py
+++ src/mia/analysis.py
@@ -4,10 +4,13 @@ a dataset of images.
 
 import functools
 import logging
+import re
 import numpy as np
 import pandas as pd
 
 from sklearn import manifold, preprocessing
+from mia.features.blobs import blob_props
+from mia.features.intensity import intensity_props
 
 logger = logging.getLogger(__name__)
 
@@ -62,118 +65,45 @@ def _cluster_measure(group):
     return distances.mean()
 
 
-# def feature_statistics(raw_detections):
-#     """Create blob features from a file of blob detections
-#
-#     :param csv_file: file containing the detected blobs
-#     :param output_file: name to output the resulting features to.
-#     """
-#     image_names = raw_detections['image_name'].unique()
-#
-#     info_df = _create_info_data_frame(raw_detections, image_names)
-#     feature_matrix = _create_feature_matrix(raw_detections, image_names)
-#
-#     feature_matrix = pd.concat([feature_matrix, info_df], axis=1)
-#     return feature_matrix
-#
-#
-# def load_synthetic_meta_data(file_name):
-#     labels = pd.DataFrame.from_csv(file_name)
-#
-#     regex_string = r"Output-test_Mix_DPerc(\d+)_c-02_26_2015-\d+_\d+_\d+"
-#     name_regex = re.compile(regex_string)
-#     group_names = [l for l in labels.index]
-#     group_ids = [int(re.match(name_regex, name).group(1))
-#                  for name in group_names]
-#     labels.index = group_ids
-#     return labels
-#
-#
-# def create_meta_data_for_synthetic_mammogram(data_frame, labels):
-#     ids = [[group_id for name in data_frame.img_name
-#             if "DPerc%d" % group_id in name]
-#            for group_id in labels.index]
-#
-#     ids_by_group = pd.Series([i for row in ids for i in row],
-#                              index=data_frame.img_name)
-#     cls_by_group = pd.Series([labels.BIRADS[i] for row in ids for i in row],
-#                              index=data_frame.img_name)
-#
-#     return pd.DataFrame({'group_id': ids_by_group, 'class': cls_by_group})
-#
-#
-# def _create_info_data_frame(raw_detections, index_names):
-#     info_columns = ['patient_id', 'view', 'side', 'class']
-#     info_df = raw_detections[info_columns].drop_duplicates()
-#     info_df.index = index_names
-#     return info_df
-#
-#
-# def _create_feature_matrix(raw_detections, index_names):
-#     feature_matrix = pd.DataFrame()
-#     for index, frame in raw_detections.groupby('image_name'):
-#         shape_props = blob_props(frame)
-#         # int_props = intensity_props(frame)
-#         row = shape_props
-#         # row = pd.concat([shape_props, int_props], axis=1)
-#         feature_matrix = pd.concat([feature_matrix, row], ignore_index=True)
-#
-#     feature_matrix.index = index_names
-#     return feature_matrix
+def create_hologic_meta_data(df):
+    data = [_split_hologic_img_name(img_name) for img_name in df.index.values]
+    return pd.DataFrame(data, index=df.index, columns=['patient_id', 'side',
+                                                       'view'])
 
-# def _make_image_data_frame(img_name, df):
-#     info_df = _make_image_info_data_frame(img_name)
-#     info_df = pd.concat([info_df]*df.shape[0], ignore_index=True)
-#     frame = pd.concat([df, info_df], axis=1)
-#     return frame
-#
-#
-# def _make_image_info_data_frame(img_name):
-#     _, ext = os.path.splitext(img_name)
-#
-#     if ext == '.dcm':
-#         return _make_synthetic_info_data_frame(img_name)
-#     else:
-#         return _make_mammogram_info_data_frame(img_name)
-#
-#
-# def _make_synthetic_info_data_frame(img_name):
-#     return pd.DataFrame([img_name], columns=['img_name'])
-#
-#
-# def _make_mammogram_info_data_frame(img_name):
-#     name_regex = re.compile(r'p(\d{3}-\d{3}-\d{5})-([a-z]{2})\.png')
-#
-#     def find_image_info(name):
-#         m = re.match(name_regex, name)
-#         patient_id = m.group(1).replace('-', '')
-#         view, side = list(m.group(2))
-#         return [img_name, patient_id, view, side]
-#
-#     image_info = find_image_info(img_name)
-#     return pd.DataFrame([image_info], columns=['image_name', 'patient_id',
-#                                                'view', 'side'])
-#
-#
-# def add_BIRADS_class(feature_matrix, class_labels_file):
-#     """Add the BIRADS classes to the data frame given a file with the class
-#     labels
-#
-#     :param feature_matrix: DataFrame for features where the index is the imag
-# e
-#                            names
-#     :param class_labels_file: csv file containg the class labels
-#     :returns: DataFrame with the class labels added under the column 'class'
-#     """
-#     class_labels = pd.DataFrame().from_csv(class_labels_file)
-#     class_labels = class_labels['BI-RADS']
-#
-#     class_hash = {}
-#     for img, c in class_labels.iteritems():
-#         class_hash[img] = c
-#
-#     img_classes = [class_hash[int(name)]
-#                    for name in feature_matrix['patient_id']]
-#     feature_matrix['class'] = pd.Series(img_classes,
-#                                         index=feature_matrix.index)
-#     return feature_matrix
+
+def _split_hologic_img_name(name):
+    img_regex = re.compile(r'p(\d{3}-\d{3}-\d{5})-([a-z])([a-z])\.png')
+    m = re.match(img_regex, name)
+    return m.groups()
+
+
+def create_synthetic_meta_data(df, meta_data_file):
+    indicies = [_split_sythentic_img_name(img_name)
+                for img_name in df.index.values]
+    raw_md = pd.DataFrame.from_csv(meta_data_file)
+    md = raw_md.loc[indicies]
+    md['phantom_name'] = md.index
+    md.index = df.index
+    return md
+
+
+def _split_sythentic_img_name(name):
+    group_regex = re.compile(r'(test_Mix_DPerc\d+_c)_\d+\.dcm')
+    img_regex = re.compile(r'(phMix\d+)_c_\d+\.dcm')
+
+    group_match = re.match(group_regex, name)
+    img_match = re.match(img_regex, name)
+    if group_match:
+        return group_match.group(1)
+    elif img_match:
+        return img_match.group(1)
+
+
+def features_from_blobs(df):
+    features = df.groupby(df.index).apply(blob_props)
+    return features.reset_index(level=1, drop=True)
+
+
+def features_from_intensity(df):
+    features = df.groupby(df.index).apply(intensity_props)
+    return features.reset_index(level=1, drop=True)
--- src/tests/unit_tests/analysis_test.py
+++ src/tests/unit_tests/analysis_test.py
@@ -61,3 +61,49 @@ class AnalysisTests(unittest.TestCase):
         nose.tools.assert_equal(mapping.shape, (360, 2))
         nose.tools.assert_equal(distances.shape, (90, ))
         nose.tools.assert_almost_equal(distances.mean(), 5, delta=1.0)
+
+    def test_create_hologic_meta_data(self):
+        md = create_hologic_meta_data(self._features)
+
+        nose.tools.assert_equal(md.shape, (self._features.shape[0], 3))
+        np.testing.assert_array_equal(md.columns.values, ['patient_id',
+                                                          'side', 'view'])
+
+    def test_create_hologic_meta_data_raw(self):
+        path = get_file_path("2015-03-05-results.csv")
+        df = pd.DataFrame.from_csv(path)
+        df.index = df.image_name
+        md = create_hologic_meta_data(df)
+
+        nose.tools.assert_equal(md.shape, (df.shape[0], 3))
+        np.testing.assert_array_equal(md.columns.values, ['patient_id',
+                                                          'side', 'view'])
+
+    def test_create_synthetic_meta_data(self):
+        blobs_path = get_file_path("synthetic_blobs.csv")
+        df = pd.DataFrame.from_csv(blobs_path)
+        df.index = df['img_name']
+
+        md_path = get_file_path("synthetic_meta_data_cleaned.csv")
+        md = create_synthetic_meta_data(df, md_path)
+
+        nose.tools.assert_equal(md.shape, (df.shape[0], 17))
+        np.testing.assert_equal(md.index.values, df.index.values)
+
+    def test_features_from_blobs(self):
+        path = get_file_path("2015-03-05-results.csv")
+        df = pd.DataFrame.from_csv(path)
+        df.index = df.image_name
+
+        features = features_from_blobs(df)
+
+        nose.tools.assert_equal(features.shape, (360, 12))
+
+    def test_features_from_intensity(self):
+        path = get_file_path("2015-03-05-results.csv")
+        df = pd.DataFrame.from_csv(path)
+        df.index = df.image_name
+
+        features = features_from_intensity(df)
+
+        nose.tools.assert_equal(features.shape, (360, 8))

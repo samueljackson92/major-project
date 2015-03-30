6147f92203c7005e356f43311c8765c60da7212b
Samuel Jackson <samueljackson@outlook.com>
2015-03-30 18:11:56 +0100

    Minor changes to mapping viz dump function.

--- src/mia/io_tools.py
+++ src/mia/io_tools.py
@@ -50,24 +50,29 @@ def check_is_file(img_path, *exts):
                                              for ext in exts])
 
 
-def dump_mapping_to_json(mapping, columns, output_file):
+def dump_mapping_to_json(mapping, columns, class_labels, output_file):
     """Dump a mapping to JSON data.
 
     :param mapping: the data frame to store
     :param columns: the columns to use
     :param output_file: the output file to store it in
     """
-    groups = mapping.groupby('class')
+    colours = ['#0035E5', '#00D86E', '#97CB00', '#BF0000']
+    groups = mapping.groupby(class_labels)
     json_data = []
-    for index, group in groups:
+    for i, (class_label, group) in enumerate(groups):
         points = group[columns].as_matrix().tolist()
         img_names = group.index.values.tolist()
         point_data = zip(points, img_names)
         data = [{'x': x, 'y': y, 'name': name} for (x, y), name in point_data]
 
         g = {
-            'name': "BI-RADS Class %d" % index,
+            'name': "Class: %d" % class_label,
+            'color': colours[i],
             'data': data,
+            'marker': {
+                'symbol': 'circle'
+            }
         }
         json_data.append(g)
 

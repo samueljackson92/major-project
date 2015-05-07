8db7e869867968686863945bfbb7ea7d3a1c8548
Samuel Jackson <samueljackson@outlook.com>
2015-05-07 16:12:05 +0100

    Simplfy CLI

--- src/tests/unit_tests/command_test.py
+++ src/tests/unit_tests/command_test.py
@@ -11,28 +11,3 @@ def test_prints_version():
     result = runner.invoke(command.cli, ['--version'])
     nose.tools.assert_equal(result.exit_code, 0)
     nose.tools.assert_true('mia, version' in result.output)
-
-
-def test_analysis():
-    runner = CliRunner()
-    path = get_file_path("blob_detection.csv")
-    columns = ['avg_radius', 'std_radius', 'blob_count']
-    result = runner.invoke(command.tSNE, [path] + columns)
-
-    nose.tools.assert_equal(result.exit_code, 0)
-
-# 
-# def test_analysis_saves_to_file():
-#     runner = CliRunner()
-#     path = get_file_path("blob_detection.csv")
-#     with open(path, 'r') as f:
-#         contents = f.read()
-#
-#     with runner.isolated_filesystem():
-#         with open('blob_detection.csv', 'w') as f:
-#             f.write(contents)
-#
-#         args = ['blob_detection.csv', '--output-file=output.csv']
-#         result = runner.invoke(command.tSNE, args)
-#         nose.tools.assert_equal(result.exit_code, 0)
-#         nose.tools.assert_true(os.path.isfile('output.csv'))

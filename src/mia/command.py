3e52ec7a8db3f3392f03a25df6d175e91c6a1c53
Samuel Jackson <samueljackson@outlook.com>
2015-05-07 16:17:58 +0100

    Update documentation on CLI.

--- src/mia/command.py
+++ src/mia/command.py
@@ -23,6 +23,10 @@ LOG_LEVELS = {
               type=click.Choice(['debug', 'info', 'warning']),
               help="Level of logging to report")
 def cli(log_level):
+    """MIA: Mammogram Image Analysis
+
+    MIA is a python library for analysing mammographic image data.
+    """
     logger.setLevel(LOG_LEVELS[log_level])
 
 
@@ -46,6 +50,7 @@ def reduction():
 @click.option('--num-processes', default=2,
               help="Num of processes to use for the reduction.")
 def blob(image_directory, masks_directory, output_file, num_processes):
+    """Detect blobs over a dataset of images"""
     reduction = BlobFeaturesReduction(image_directory, masks_directory)
     features = reduction.reduce(num_processes=num_processes)
     features.to_csv(output_file)
@@ -58,6 +63,7 @@ def blob(image_directory, masks_directory, output_file, num_processes):
 @click.option('--num-processes', default=2,
               help="Num of processes to use for the reduction.")
 def line(image_directory, masks_directory, output_file, num_processes):
+    """Detect lines over a dataset of images"""
     reduction = LineFeaturesReduction(image_directory, masks_directory)
     features = reduction.reduce(num_processes=num_processes)
     features.to_csv(output_file)
@@ -72,6 +78,9 @@ def line(image_directory, masks_directory, output_file, num_processes):
               help="Num of processes to use for the reduction.")
 def intensity_from_patch(image_directory, masks_directory, patch_file,
                          output_file, num_processes):
+    """Detect intensity properties from the histogram of a patch defined by
+    blobs/lines.
+    """
     patch_frame = pd.DataFrame.from_csv(patch_file)
     reduction = PatchIntensityFeaturesReduction(image_directory,
                                                 masks_directory)
@@ -88,6 +97,9 @@ def intensity_from_patch(image_directory, masks_directory, patch_file,
               help="Num of processes to use for the reduction.")
 def texture_from_patch(image_directory, masks_directory, patch_file,
                        output_file, num_processes):
+    """Detect texture properties from the histogram of a patch defined by
+    blobs/lines.
+    """
     patch_frame = pd.DataFrame.from_csv(patch_file)
     reduction = PatchTextureFeaturesReduction(image_directory,
                                               masks_directory)
@@ -99,6 +111,9 @@ def texture_from_patch(image_directory, masks_directory, patch_file,
 @click.argument('image-file', type=click.Path())
 @click.argument('mask-file', required=False, type=click.Path())
 def detect_blobs(image_file, mask_file):
+    """Detect blobs from a single image and display the detected blobs plotted
+    on an image.
+    """
     img, msk = mia.utils.preprocess_image(image_file, mask_file)
     blob_df = mia.features.blobs.detect_blobs(img, msk)
     mia.plotting.plot_blobs(img, blob_df[['x', 'y', 'radius']].as_matrix())
@@ -108,6 +123,9 @@ def detect_blobs(image_file, mask_file):
 @click.argument('image-file', type=click.Path())
 @click.argument('mask-file', required=False, type=click.Path())
 def detect_linear(image_file, mask_file):
+    """Detect lines from a single image and display the detected lines plotted
+    on an image.
+    """
     img, msk = mia.utils.preprocess_image(image_file, mask_file)
     _, line_image = mia.features.linear_structure.detect_linear(img, msk)
     img = transform.pyramid_reduce(img, 4)
